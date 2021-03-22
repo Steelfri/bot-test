9fc11ab6ad370be55754e987f5deabd897dac876
Jakob <67517786+Steelfri@users.noreply.github.com>
2021-03-22 07:33:41 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix = "!", description = "bot de test")
 
 @bot.event
-async def on_ready()
+async def on_ready():
 	print("Start")
 
 bot.run('ODIzNTE2OTU5OTA2ODU2OTkx.YFh97w.7SknaliF7na4bShsCrvnE_7NcYs')
