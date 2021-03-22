d596e253d7e251a522f48cbba19f178bd482bfaa
Jakob <67517786+Steelfri@users.noreply.github.com>
2021-03-22 07:30:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import discord
 from discord.ext import commands
 
+bot = commands.Bot(command_prefix = "!", description = "bot de test")
+
+@bot.event
+async def on_ready()
+	print("Start")
+
+bot.run('ODIzNTE2OTU5OTA2ODU2OTkx.YFh97w.7SknaliF7na4bShsCrvnE_7NcYs')
