794055e9e9a8a06f87dc316f6128bfa7d37b8c4a
Jakob <67517786+Steelfri@users.noreply.github.com>
2021-03-22 07:48:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,19 @@ bot = commands.Bot(command_prefix = "!", description = "bot de test")
 async def on_ready():
 	print("Start")
 
+@bot.command()
+async def coucou(ctx):
+	await ctx.send("Hellow there")
+
+@bot.command()
+async def serverInfo(ctx):
+	server = ctx.guild
+	numberOfChannel = len(server.text_channels)
+	numberOfVoice = len(server.voice_channels)
+	numberOfMember = server.member_count
+	name = server.name 
+	numberofchannels = numberOfChannel + numberOfVoice
+	message = f"**{name}**\nNombre de membres : *{numberOfMember}*\nNombre de channels : *{numberofchannels}*\nSalons textuels : *{numberOfChannel}*\n Salons vocaux : *{numberOfVoice}*"
+	await ctx.send(message)
+
 bot.run('ODIzNTE2OTU5OTA2ODU2OTkx.YFh97w.nHa0Mfy9i437EECuDDDjP4bkQYQ')
