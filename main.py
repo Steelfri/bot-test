b5002caabce625c41f94d6d151e265452094a0dd
Jakob <67517786+Steelfri@users.noreply.github.com>
2021-03-25 19:24:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -41,4 +41,50 @@ async def help(ctx, arg=None):
 
 		await ctx.send(embed = embed)
 
+@client.command()
+@commands.has_any_role(824784559735963688)
+async def tmute(ctx, member : discord.Member, time=0,*, reason=None):
+    if not member or time == 0 or time == str:
+        await ctx.channel.send(embed=commanderror)
+        return
+    elif reason == None:
+        reason = "Aucunes raisons fournies"
+
+    muteRole = discord.utils.get(ctx.guild.roles, id=663076470180151339)
+    await member.add_roles(muteRole)
+
+    tempMuteEmbed = discord.Embed(colour=embedcolour, description=f"**Reason:** {reason}")
+    tempMuteEmbed.set_author(name=f"{member} Has Been Muted", icon_url=f"{member.avatar_url}")
+    tempMuteEmbed.set_footer(text=embedfooter)
+
+    await ctx.channel.send(embed=tempMuteEmbed)
+
+    tempMuteModLogEmbed = discord.Embed(color=embedcolour)
+    tempMuteModLogEmbed.set_author(name=f"[MUTE] {member}", icon_url=f"{member.avatar_url}")
+    tempMuteModLogEmbed.add_field(name="User", value=f"{member.mention}")
+    tempMuteModLogEmbed.add_field(name="Moderator", value=f"{ctx.message.author}")
+    tempMuteModLogEmbed.add_field(name="Reason", value=f"{reason}")
+    tempMuteModLogEmbed.add_field(name="Duration", value=f"{str(time)}")
+    tempMuteModLogEmbed.set_footer(text=embedfooter)
+    modlog = client.get_channel(638783464438759464)
+    await modlog.send(embed=tempMuteModLogEmbed)
+
+    tempMuteDM = discord.Embed(color=embedcolour, title="Mute Notification", description="You Were Muted In **The Official Vanmanyo Discord Server**")
+    tempMuteDM.set_footer(text=embedfooter)
+    tempMuteDM.add_field(name="Reason", value=f"{reason}")
+    tempMuteDM.add_field(name="Duration", value=f"{time}")
+
+    userToDM = client.get_user(member.id)
+    await userToDM.send(embed=tempMuteDM)
+
+    await asyncio.sleep(time)
+    await member.remove_roles(muteRole)
+
+    unMuteModLogEmbed = discord.Embed(color=embedcolour)
+    unMuteModLogEmbed.set_author(name=f"[UNMUTE] {member}", icon_url=f"{member.avatar_url}")
+    unMuteModLogEmbed.add_field(name="User", value=f"{member.mention}")
+    unMuteModLogEmbed.set_footer(text=embedfooter)
+    modlog = client.get_channel(638783464438759464)
+    await modlog.send(embed=unMuteModLogEmbed)
+
 bot.run('ODIzNTE2OTU5OTA2ODU2OTkx.YFh97w.gch47GAXlnIhoGju2JvymnEd0Ps')
