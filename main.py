227a66bf3fb0adddafe07990e3496d311da79131
Jakob <67517786+Steelfri@users.noreply.github.com>
2021-03-25 17:44:32 -0400

    Update main.py

--- main.py
+++ main.py
@@ -8,18 +8,18 @@ async def on_ready():
 	print("Start")
 
 @bot.command()
-async def help(ctx, help):
-	embed=discord.Embed(title="Liste des commandes", url="https://steelfri.fr", description="Voici la liste des commandes du bot :\n", color=0x4cf6eb)
-	embed.set_footer(text="Esclavre de Steelfri - Communauté Steelfri / Team 031", icon_url = "https://media.discordapp.net/attachments/736631083185078302/824098862783397928/image0.png?width=559&height=559")
-	embed.add_field(name="Modération", value="`*help moderation`", inline=True)
-	embed.add_field(name="Fun", value="`*help fun`", inline=True)
-	embed.add_field(name="Autres", value="`*help others`", inline=True)
-	embed.add_field(name="ㅤ", value="[Twitch](https://twitch.tv/steelfri)", inline=True)
-	embed.add_field(name="ㅤ", value="[Twitter](https://twitter.com/031_steelfri/)", inline=True)
+async def help(ctx, arg):
+	if arg == "":
+		embed=discord.Embed(title="Liste des commandes", url="https://steelfri.fr", description="Voici la liste des commandes du bot :\n", color=0x4cf6eb)
+		embed.set_footer(text="Esclavre de Steelfri - Communauté Steelfri / Team 031", icon_url = "https://media.discordapp.net/attachments/736631083185078302/824098862783397928/image0.png?width=559&height=559")
+		embed.add_field(name="Modération", value="`*help moderation`", inline=True)
+		embed.add_field(name="Fun", value="`*help fun`", inline=True)
+		embed.add_field(name="Autres", value="`*help others`", inline=True)
+		embed.add_field(name="ㅤ", value="[Twitch](https://twitch.tv/steelfri)", inline=True)
+		embed.add_field(name="ㅤ", value="[Twitter](https://twitter.com/031_steelfri/)", inline=True)
 
-	await ctx.send(embed = embed)
-
-async def help(ctx, test):
-	await ctx.send("test")
+		await ctx.send(embed = embed)
+	elif arg == "test":
+		await ctx.send("test")
 
 bot.run('ODIzNTE2OTU5OTA2ODU2OTkx.YFh97w.B7JuSssBplV59Yl4sBDddHARN3M')
