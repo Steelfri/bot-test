09ef56b977d79ed326054e1c222a2868832e01e8
Jakob <67517786+Steelfri@users.noreply.github.com>
2021-03-25 18:00:36 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ async def on_ready():
 @bot.command()
 async def help(ctx, arg=None):
 	if arg == "moderator":
-		embed=discord.Embed(title="Liste des commandes", url="https://steelfri.fr", description="Voici la liste des commandes du bot :\n", color=0x4cf6eb)
+		embed=discord.Embed(title="Liste des commandes de modéartion", url="https://steelfri.fr", description="Voici la liste des commandes destinées aux modérateurs :\n", color=0x4cf6eb)
 		embed.set_footer(text="Esclave de Steelfri - Communauté Steelfri / Team 031", icon_url = "https://media.discordapp.net/attachments/736631083185078302/824098862783397928/image0.png?width=559&height=559")
 		embed.add_field(name="Modération :", value="`*tmute`, `*tban`, `*ban`, `*kill`", inline=False)
 		
