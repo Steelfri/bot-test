8c92cc8e73fcba4d3ccee7367a13585b2fdaccd3
Jakob <67517786+Steelfri@users.noreply.github.com>
2021-03-25 08:15:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,20 @@ import discord
 from discord.ext import commands
 
 bot = commands.Bot(command_prefix = "*", description = "bot de test")
+client.remove_command('help')
 
 @bot.event
 async def on_ready():
 	print("Start")
 
 @bot.command()
-async def commands(ctx):
+async def help(ctx):
 	embed=discord.Embed(title="Liste des commandes", url="https://steelfri.fr", description="Voici la liste des commandes du bot :\n", color=0x4cf6eb)
 	embed.set_footer(text="Esclavre de Steelfri - Communauté Steelfri / Team 031", icon_url = "https://media.discordapp.net/attachments/736631083185078302/824098862783397928/image0.png?width=559&height=559")
 	embed.add_field(name="Modération", value="`*help moderation`", inline=True)
 	embed.add_field(name="Fun", value="`*help fun`", inline=True)
 	embed.add_field(name="Autres", value="`*help others`", inline=True)
 	embed.add_field(name="ㅤ", value="[Twitch](https://twitch.tv/steelfri)", inline=True)
-	embed.add_field(name="ㅤ", value="ㅤ", inline=True)
 	embed.add_field(name="ㅤ", value="[Twitter](https://twitter.com/031_steelfri/)", inline=True)
 
 	await ctx.send(embed = embed)
