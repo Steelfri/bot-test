c454c6746978ea71535dbc09bde96bc45606576b
Jakob <67517786+Steelfri@users.noreply.github.com>
2021-03-26 17:31:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ async def tmute(ctx, member: discord.Member, time: int, d, *, reason=None):
 
 				await member.remove_roles(role)
 
-			uuembed=discord.Embed(title="Unmute", url="https://steelfri.fr", description=f"{member} a été mute :\n", color=0x4cf6eb)
+			uembed=discord.Embed(title="Unmute", url="https://steelfri.fr", description=f"{member} a été mute :\n", color=0x4cf6eb)
 			uembed.set_footer(text="Esclave de Steelfri - Communauté Steelfri / Team 031", icon_url = "https://media.discordapp.net/attachments/736631083185078302/824098862783397928/image0.png?width=559&height=559")
 			uembed.add_field(name="Membre Unmute :", value=f"`{member.mention}`", inline=True)
 			await ctx.send(embed=uembed)
