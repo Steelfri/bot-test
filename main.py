d5305d99c5b5720426ecdf4759b6336a03f2969a
Jakob <67517786+Steelfri@users.noreply.github.com>
2021-03-26 19:30:41 -0400

    Update main.py
    by them again

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ intents.members = True
 bot = commands.Bot(command_prefix="*", description="bot de test", help_command=None, intents=intents)
 
 muted_list = []
+banned_list = []
 
 @bot.event
 async def on_ready():
@@ -18,6 +19,10 @@ async def on_member_join(member):
     if member.id in muted_list:
         for role in member.guild.roles:
             if role.name == "Muted":
+                await member.add_roles(role)
+    if member.id in banned_list:
+        for role in member.guild.roles:
+            if role.name == "Banned":
                 await member.add_roles(role)
 
 
@@ -127,6 +132,74 @@ async def tmute(ctx, member: discord.Member, time: int, aac, *, reason=None):
             uembed.set_footer(text="Esclave de Steelfri - Communauté Steelfri / Team 031",
                               icon_url="https://media.discordapp.net/attachments/736631083185078302/824098862783397928/image0.png?width=559&height=559")
             uembed.add_field(name="Membre Unmute :", value=f"`{member}`", inline=True)
+            await ctx.send(embed=uembed)
+
+            return
+
+
+@bot.command()
+@commands.has_any_role(824784559735963688)
+async def tban(ctx, member: discord.Member, time: int, aac, *, reason=None):
+    guild = ctx.guild
+    global muted_list
+
+    if aac not in ["s", "m", "h", "d"]:
+        eembed = discord.Embed(title="Erreur", url="https://steelfri.fr", description=f"Une erreur est survenue:\n",
+                               color=0x4cf6eb)
+        eembed.set_footer(text="Esclave de Steelfri - Communauté Steelfri / Team 031",
+                          icon_url="https://media.discordapp.net/attachments/736631083185078302/824098862783397928/image0.png?width=559&height=559")
+        eembed.add_field(name="Erreur :", value=f"Un champ est mal entré", inline=True)
+        return await ctx.send(embed=eembed)
+
+    if member.id in muted_list:
+        eembed = discord.Embed(title="Erreur", url="https://steelfri.fr", description=f"Une erreur est survenue:\n",
+                               color=0x4cf6eb)
+        eembed.set_footer(text="Esclave de Steelfri - Communauté Steelfri / Team 031",
+                          icon_url="https://media.discordapp.net/attachments/736631083185078302/824098862783397928/image0.png?width=559&height=559")
+        eembed.add_field(name="Erreur :", value=f"Le membre est déjà mute", inline=True)
+        return await ctx.send(embed=eembed)
+
+    for role in guild.roles:
+        if role.name == "Banned":
+            await member.add_roles(role)
+            banned_list.append(member.id)
+
+            tembed = discord.Embed(title="Ban Temporaire", url="https://steelfri.fr",
+                                   description=f"{member} a été mute :\n", color=0x4cf6eb)
+            tembed.set_footer(text="Esclave de Steelfri - Communauté Steelfri / Team 031",
+                              icon_url="https://media.discordapp.net/attachments/736631083185078302/824098862783397928/image0.png?width=559&height=559")
+            tembed.add_field(name="Membre Ban :", value=f"`{member}`", inline=True)
+            tembed.add_field(name="Raison :", value=f"`{reason}`", inline=True)
+            await ctx.send(embed=tembed)
+
+            if aac == "s":
+                await asyncio.sleep(time)
+
+            elif aac == "m":
+                await asyncio.sleep(time * 60)
+
+            elif aac == "h":
+                await asyncio.sleep(time * 60 * 60)
+
+            elif aac == "d":
+                await asyncio.sleep(time * 60 * 60 * 24)
+
+            else:
+                eembed = discord.Embed(title="Erreur", url="https://steelfri.fr",
+                                       description=f"Une erreur est survenue:\n", color=0x4cf6eb)
+                eembed.set_footer(text="Esclave de Steelfri - Communauté Steelfri / Team 031",
+                                  icon_url="https://media.discordapp.net/attachments/736631083185078302/824098862783397928/image0.png?width=559&height=559")
+                eembed.add_field(name="Erreur :", value=f"Un champ est mal entré", inline=True)
+                await ctx.send(embed=eembed)
+
+            await member.remove_roles(role)
+            banned_list.remove(member.id)
+
+            uembed = discord.Embed(title="Unmute", url="https://steelfri.fr", description=f"{member} a été mute :\n",
+                                   color=0x4cf6eb)
+            uembed.set_footer(text="Esclave de Steelfri - Communauté Steelfri / Team 031",
+                              icon_url="https://media.discordapp.net/attachments/736631083185078302/824098862783397928/image0.png?width=559&height=559")
+            uembed.add_field(name="Membre Unban :", value=f"`{member}`", inline=True)
             await ctx.send(embed=uembed)
 
             return
